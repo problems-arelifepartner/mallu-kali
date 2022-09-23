797af0b11f0b6c9464be8f4786b65427b09c5581
GH05T HUNTER5 <108191615+GH05T-HUNTER5@users.noreply.github.com>
2022-09-23 18:42:18 +0530

    Update mallu-kali.sh

--- mallu-kali.sh
+++ mallu-kali.sh
@@ -65,6 +65,30 @@ clear
 delepo-mrd3vil
 fi
 }
+function gh05t-kali () {
+banners-logo
+sleep 2
+pkg install wget -y
+pkg install python -y
+wget https://raw.githubusercontent.com/GH05T-HUNTER5/kali-havk/main/install.sh
+if [ -f install.sh ]; then
+echo ""
+clear
+banners-logo
+echo "  please wait..."
+sleep 5
+bash install.sh
+else
+echo ""
+banners-logo
+echo "  please wait..."
+sleep 5
+pkg install wget
+pkg install wget -y && wget https://raw.githubusercontent.com/GH05T-HUNTER5/kali-havk/main/install.sh && bash install.sh
+fi
+}
+
+
 
 
 
